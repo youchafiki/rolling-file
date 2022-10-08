802ffa3c7705e9fab4aca628ac0e1406b26b6448
youssef_chafiki <youssef_chafiki@carrefour.com>
2022-10-08 16:22:02 +0200

    add roling file and a main class

---
+++ rollingfile/src/main/java/com/logging/rollingfile/Log4j2RollingFile.java
@@ -0,0 +1,20 @@
+package com.logging.rollingfile;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Logger;
+
+@Log4j2
+public class Log4j2RollingFile {
+
+	public static void main(String[] args) throws InterruptedException {
+
+			for(int i = 0; i < 2000; i++) {
+				log.info("This is the " + i + " time I say 'Hello World'.");
+				log.warn("This is the " + i + " time I say 'warn World'.");
+				log.warn("This is the " + i + " time I say 'error World'.");
+
+				Thread.sleep(100);
+			}
+
+	}
+}
